c95aee1a341fea043b9bb51ddf89353fd3e3817c
Clickmis <Clickmis.info@gmail.com>
2019-01-18 19:08:07 +0330

    Initiallize Project

---
+++ catalog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'catalog'
+
+urlpatterns = [
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('(?P<pk>[0-9]+)/', views.detail, name='detail'),
+    # re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # re_path(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # re_path(r'^(?P<questionId>[0-9]+)/vote/$', views.vote, name='vote'),
+]
